b7ce767df6b3e6fa5bae1e6d38105904a52cf864
Florent Dueme <florent.dueme@orange.fr>
2020-11-10 11:54:04 +0100

    Added text_to_speech and requirements file

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import speech_recognition as sr
 from time import ctime
 import time
 import webbrowser
+import os
+import random
+import playsound
+from gtts import gTTS
+
 
 # First we will initialize a recognizer which is the main component of this library
 # It is the think that actually recognizes speech
@@ -17,34 +22,44 @@ r = sr.Recognizer()
 def record_audio(ask=False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            alex_speak(ask)
         audio = r.listen(source)
         voice_data = ""
         try:
             voice_data = r.recognize_google(audio)
             print(voice_data)
         except sr.UnknownValueError:
-            print("Sorry, I did not understand")
+            alex_speak("Sorry, I did not understand")
         except sr.RequestError:
-            print("Sorry, my speech service is down")
+            alex_speak("Sorry, my speech service is down")
         return voice_data
+
+
+def alex_speak(audio_string):
+    tts = gTTS(text=audio_string, lang="en")
+    r = random.randint(1, 1000000)
+    audio_file = "audio-" + str(r) + ".mp3"
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
 
 
 def respond(voice_data):
     if "What is your name" in voice_data:
-        print("My name is Alex")
+        alex_speak("My name is Alex")
     if "What time is it" in voice_data:
-        print(ctime())
+        alex_speak(ctime())
     if "search" in voice_data:
         search = record_audio("What do you want to search for?")
         url = "https://google.com/search?q=" + search
         webbrowser.get().open(url)
-        print("Here is what I found for" + search)
+        alex_speak("Here is what I found for" + search)
     if "find location" in voice_data:
         location = record_audio("What is the location?")
         url = "https://google.nl/maps/place/" + location + "/&amp;"
         webbrowser.get().open(url)
-        print("Here is the location of" + location)
+        alex_speak("Here is the location of" + location)
     if "exit" in voice_data:
         exit()
 
